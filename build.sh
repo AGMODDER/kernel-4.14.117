fb1fa4cfa399d38f57248749b97d30e6293d8ee6
S4muel007 <milmadara65@gmail.com>
2021-11-17 16:46:17 +0000

    Revert "drivers: thermal: limits-dcvs: Always build driver"
    This reverts commit ef7c4f779e13f334ae655faf0387635667219640.

---
+++ build.sh
@@ -0,0 +1,8 @@
+config=vendor/sofiar_defconfig
+
+export PATH="/home/milmadara65/toolchains/clang-r428724/bin:/home/milmadara65/toolchains/aarch64-linux-android-4.9/bin:/home/milmadara65/toolchains/arm-linux-androideabi-4.9/bin:$PATH"
+
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+export CLANG_TRIPLE=aarch64-linux-gnu-
